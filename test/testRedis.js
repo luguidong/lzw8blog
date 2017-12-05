3ab8981869e87176958a64b0f113e7a5a177f584
luguidong <920430764@qq.com>
2017-12-05 20:35:56 +0800

    测试redis

--- test/testRedis.js
+++ test/testRedis.js
@@ -1,9 +1,24 @@
-
 var redis = require('redis');
 var RDS_PORT = 6379,
     RDS_HOST = '127.0.1.1',
-    RDS_OPTS = {},//配置项
+    RDS_OPTS = {}, //配置项
     client = redis.createClient(RDS_PORT, RDS_HOST, RDS_OPTS);
+
+
+client.on('connect', () => {
+    // client.set('auth', 'roam', redis.print);
+    // client.get('auth', redis.print);
+    client.hmset('short', { 'a': '111', 'b': 'wf', 'c': 'hl' }, redis.print);
+    client.hgetall('short', (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(res);
+        }
+    })
+
+    console.log('connect');
+})
 client.on('ready', function (err) {
     console.log('ready');
 })
