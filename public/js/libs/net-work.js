829605a0f328304102fe49c211f2af8742512dba
d-melon <15286749996>
2018-01-06 16:26:09 +0800

    post不需要取消跨域

--- public/js/libs/net-work.js
+++ public/js/libs/net-work.js
@@ -5,27 +5,24 @@ const install = (Vue) => {
             get: (url, data, successCallback) => {
                 console.log(data);
                 axios.get(url, { params: data }).
-                then((serverData) => {
-                    console.log(serverData);
-                    successCallback(serverData.data);
-                }).
-                catch((err) => {
-                    console.log('请求错误' + err);
-                })
+                    then((serverData) => {
+                        console.log(serverData);
+                        successCallback(serverData.data);
+                    }).
+                    catch((err) => {
+                        console.log('请求错误' + err);
+                    })
             },
             post: (url, data, successCallback) => {
                 //如果是测试环境开发，需要跨域请求服务器的数据
-                if (true) {
-                    url = "http://localhost:3000" + url;
-                }
                 console.log('url');
                 axios.post(url, data).
-                then((data) => {
-                    successCallback(data.data);
-                }).
-                catch((err) => {
-                    console.log('请求错误' + err);
-                })
+                    then((data) => {
+                        successCallback(data.data);
+                    }).
+                    catch((err) => {
+                        console.log('请求错误' + err);
+                    })
             }
         }
 
