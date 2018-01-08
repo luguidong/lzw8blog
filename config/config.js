f6e4f5b717a55f633ce342477d710fdc7963dd4e
luguidong <920430764@qq.com>
2018-01-08 21:27:03 +0800

    环境配置

--- config/config.js
+++ config/config.js
@@ -1,23 +1,18 @@
 const defaultConfig = './config-default.js';
-const overrideConfig = './config-override.js';
+const prodConfig = './config-override.js';
 const testConfig = './config-test.js';
 
 const fs = require('fs');
 var config = null;
-
-if(process.env.NODE_ENV === 'test'){
+console.log('当前环境' + process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'test') {
     console.log('load testconfig');
     config = require(testConfig);
-}else{
-    console.log('load default config');
+} else if (process.env.NODE_ENV === 'production') {
+    console.log('load prodConfig');
+    config = require(prodConfig);
+} else {
+    console.log('load defaultConfig');
     config = require(defaultConfig);
-    try{
-        if(fs.statSync(overrideConfig).isFile()){
-            console.log('load overrideconfig');
-            config = Object.assign(config,require(overrideConfig));
-        }
-    }catch(err){
-        console.log('cannot load config');
-    }
 }
 module.exports = config;
