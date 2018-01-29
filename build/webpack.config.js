f45fedbec3dd007715e464749fb2f6cff2eee38c
luguidong <920430764@qq.com>
2018-01-29 21:24:41 +0800

    测试第三方资源包不打包

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -16,40 +16,40 @@ const config = {
     },
     module: {
         rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        css: ExtractTextPlugin.extract({
-                            use: 'css-loader?minimize',
-                            fallback: 'vue-style-loader'
-                        })
-                    }
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {
+                    css: ExtractTextPlugin.extract({
+                        use: 'css-loader?minimize',
+                        fallback: 'vue-style-loader'
+                    })
                 }
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    "presets": [
-                        ['es2015', { modules: false }]
-                    ],
-                    plugins: ['syntax-dynamic-import']
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader',
-                    fallback: 'style-loader'
-                })
-            },
-            {
-                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-                loader: 'url-loader?limit=800000' //限制大小，如果大小小于1k，则转为base64
-            }]
+            }
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+                "presets": [
+                    ['es2015', { modules: false }]
+                ],
+                plugins: ['syntax-dynamic-import']
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader',
+                fallback: 'style-loader'
+            })
+        },
+        {
+            test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+            loader: 'url-loader?limit=800000' //限制大小，如果大小小于1k，则转为base64
+        }]
     },
     plugins: [
         new ExtractTextPlugin('main.css'),
